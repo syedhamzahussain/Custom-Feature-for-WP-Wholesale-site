e571650843533e376e09d969ef019daf6b17cf34
Umer Solcoders <umer@solcoders.com>
2022-11-08 18:47:04 +0500

    Changed Styling

--- templates/my-orders-cards.php
+++ templates/my-orders-cards.php
@@ -5,32 +5,36 @@
 <main>
   <ul class="box-info">
     <li>
-      <i class="bx bxs-offer"></i>
-      <span class="text">
-        <h3><?= $total_quotes ?></h3>
+    <span class="text">
         <p>Total Requested Quotes</p>
+        <h3><?= $total_quotes ?></h3>
       </span>
+      <i class="bx bxs-offer"></i>
+      
     </li>
     <li>
-      <i class="bx bx-money"></i>
-      <span class="text">
-        <h3><?= $total_amount ?></h3>
+    <span class="text">
         <p>Total Spent</p>
+        <h3><?= $total_amount ?></h3>
       </span>
+      <i class="bx bx-money"></i>
+      
     </li>
     <li>
-      <i class="bx bxs-shopping-bag-alt"></i>
-      <span class="text">
-        <h3><?= $total_pending ?></h3>
+    <span class="text">
         <p>Total Pending Orders</p>
+        <h3><?= $total_pending ?></h3>
       </span>
+      <i class="bx bxs-shopping-bag-alt"></i>
+     
     </li>
     <li>
-      <i class="bx bx-receipt"></i>
-      <span class="text">
-        <h3><?= $total_completed ?></h3>
+    <span class="text">
         <p>Total Completed Orders</p>
+        <h3><?= $total_completed ?></h3>
       </span>
+      <i class="bx bx-receipt"></i>
+      
     </li>
   </ul>
 </main>
